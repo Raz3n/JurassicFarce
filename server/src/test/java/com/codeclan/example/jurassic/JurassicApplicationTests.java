d6b7d54d471e32d27f56426036a70c7862b1e179
Morgaine <morgainedv@gmail.com>
2020-02-02 15:23:45 +0000

    adds derived queries for dinosaur custom and park custom

--- server/src/test/java/com/codeclan/example/jurassic/JurassicApplicationTests.java
+++ server/src/test/java/com/codeclan/example/jurassic/JurassicApplicationTests.java
@@ -2,7 +2,9 @@ package com.codeclan.example.jurassic;
 
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 class JurassicApplicationTests {
