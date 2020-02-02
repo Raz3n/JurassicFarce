499c958aa2b80b4fdf12499f44cd123607521149
Morgaine <morgainedv@gmail.com>
2020-02-02 15:55:26 +0000

    adds carnivore model

---
+++ server/src/main/java/com/codeclan/example/jurassic/models/Carnivore.java
@@ -0,0 +1,8 @@
+package com.codeclan.example.jurassic.models;
+
+public class Carnivore extends Dinosaur {
+
+    public Carnivore (int stomachCapacity, String species, String name, char sex, String image, Paddock paddock) {
+        super(stomachCapacity, species, name, sex, image, paddock);
+    }
+}
