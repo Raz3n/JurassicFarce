43ecf2ccc008cf19dc8470540d33bc80091b137c
Morgaine <morgainedv@gmail.com>
2020-02-03 14:20:24 +0000

    adds paddock item basic functionality

--- client/src/components/paddocks/PaddockItem.js
+++ client/src/components/paddocks/PaddockItem.js
@@ -1,2 +1,26 @@
 import React from 'react';
 import Paddock from './Paddock'; 
+import {Link} from 'react-router-dom';
+
+const PaddockItem = ({Paddock}) => {
+    if (!Paddock){
+        return "...loading paddocks"
+    }
+
+    const editURL = "/paddocks/" + paddocks.id + "/edit"
+
+    return(
+
+        <div> 
+            <Paddock paddock={paddock}/> 
+            <Link to={editURL}><button type='button'> Edit {paddock.name} </button> </Link>
+
+        </div>
+
+
+    )
+
+}
+ export default PaddockItem; 
+
+
