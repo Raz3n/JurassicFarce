15899e08bd2021079c6e8507ea83c4b86fb0b6a0
Catrin <catrinhughes92@gmail.com>
2020-02-04 17:53:48 +0000

    cannot move dinosaur to full paddock

--- client/src/components/dinosaurs/EditDinosaur.js
+++ client/src/components/dinosaurs/EditDinosaur.js
@@ -2,36 +2,21 @@ import React from 'react';
 
 const EditDinosaur = ({ paddocks, dinosaur, handleMoveDinosaur }) => {
 
-    const newPaddocks = paddocks.filter(paddock => {
-        return paddock.name !== dinosaur.paddock.name
-    })
-    const paddockList = newPaddocks.map((paddock, index) => {
-        return (
-            <option key={index}
-                value={paddock._links.self.href}>
-                {paddock.name}
-            </option>
-        )
-    }
-    )
-
     function handleSelect(e) {
         e.preventDefault()
         handleMoveDinosaur(e.target.value, dinosaur.id);
     }
 
-
-
     function filterPaddocks() {
 
-         
         const isHerbFriendly = dinosaur.species.diet === "Herbivore"
 
         return paddocks
             .filter(paddock => {
-                return paddock.name !== dinosaur.paddock.name && paddock.herbFriendly === isHerbFriendly
+                return paddock.name !== dinosaur.paddock.name &&
+                    paddock.herbFriendly === isHerbFriendly &&
+                    paddock.capacity > paddock.dinosaurs.length
             })
-    
             .map((paddock, index) => {
                 return (
                     <option key={index}
@@ -41,16 +26,13 @@ const EditDinosaur = ({ paddocks, dinosaur, handleMoveDinosaur }) => {
                 )
             })
     }
-
-
-
     return (
-            <select onChange={handleSelect}>
-                <option defaultValue={dinosaur.paddock}>
-                    {dinosaur.paddock.name}
-                </option>
-                {filterPaddocks()}
-            </select>
+        <select onChange={handleSelect}>
+            <option defaultValue={dinosaur.paddock}>
+                {dinosaur.paddock.name}
+            </option>
+            {filterPaddocks()}
+        </select>
     )
 }
 
