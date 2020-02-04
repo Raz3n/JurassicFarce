4f4e021a7ebe4bfdc8cf89bda3ad8d68ed739617
Nelson <nbritob@hotmail.com>
2020-02-04 14:27:21 +0000

    adds projection

--- server/src/main/java/com/codeclan/example/jurassic/projections/EmbedSpecies.java
+++ server/src/main/java/com/codeclan/example/jurassic/projections/EmbedSpecies.java
@@ -1,4 +1,13 @@
 package com.codeclan.example.jurassic.projections;
 
+
+import com.codeclan.example.jurassic.models.Dinosaur;
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name="embedSpecies", types=Dinosaur.class)
 public interface EmbedSpecies {
+
+    long getId();
+    int getStomachCapacity();
+    Dinosaur getDinosaur();
 }
