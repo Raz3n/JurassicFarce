e53f54d67afedc97b626e6daf56afc77b7cd3628
Morgaine <morgainedv@gmail.com>
2020-02-04 14:11:51 +0000

    adds dinosaur create form beginning

--- client/src/components/dinosaurs/DinosaurCreateForm.js
+++ client/src/components/dinosaurs/DinosaurCreateForm.js
@@ -1,41 +1,102 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Request from '../../helpers/Request';
 
-class DinosaurCreateForm extends Component{
-    constructor(props){
+class DinosaurCreateForm extends Component {
+    constructor(props) {
         super(props);
         this.state = {
-            paddocks = [],
-            name = "",
-            sex = 'F', 
-            stomach = 0,
-            stomach_capacity = 0
-            //selectedSpecies
+            paddocks: [],
+            name: "",
+            sex: "",
+            selected_paddock: "",
+            species: [],
+            selected_species: ""
         }
         this.handleName = this.handleName.bind(this);
     }
 
-
-
-    componentDidMount(){
+    componentDidMount() {
         const request = new Request();
         request.get('/paddocks').then((data) => {
-            this.setState({paddocks: data._embedded.paddocks})
-        })
+            const data = data})
+            const paddockOptions = this.data.map((paddock, index) => {
+                return (
+                    <option key={index}
+                        value={paddock._links.self.href}>
+                        {paddock.name}
+                    </option>
+                )
+            })
+            this.setState({ paddocks: paddockOptions })
+        
     }
 
-    handleName(event){
-        this.setState({name: event.target.value})
+    componentDidMount() {
+        const request = new Request();
+        request.get('/species')
+            .then((data) => {
+                this.setState({
+                    species: data._embedded.species
+                })
+            })
+    }
+
+    handleSelectedSex(event) {
+        this.setState({ sex: event.target.value })
+    }
+
+    handleName(event) {
+        this.setState({ name: event.target.value })
+    }
+
+    handlePaddockChange(event){
+        this.setState({selected_paddock: event.target.value})
     }
 
     handleSubmit(event) {
         event.preventDefault();
         const newDinosaur = {
-            name: this.state.name
+            name: this.state.name, 
+            sex: this.state.sex,
+            paddock: this.state.selected_paddock,
+            species: this.state.selected_species._links.self.href
         }
+        const request = new Request();
+        request.post('/dinosaurs', newDinosaur).then(() => {
+            window.location ='/dinosaurs'
+        })
     }
 
-
-
+    render(){
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+            <input 
+            type="text"
+            placeholder="name"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleName}
+            />
+            <input
+            type="radio"
+            name="sex"
+            value= "F"
+            onChange={this.handleSelectedSex}
+            checked={true}
+            /> Female 
+             <input type="radio"
+            name="sex"
+            value= "M"
+            onChange={this.handleSelectedSex}
+            /> Male 
+            <select name="paddocks" onChange={this.handlePaddockChange}>
+                {paddockOptions}
+            </select>
+            <button type="submit">Save new dinosaur </button>
+        </form> 
+     </div>
+        )
+    }
 
 }
