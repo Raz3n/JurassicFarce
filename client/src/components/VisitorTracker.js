40de7609f8950a0294b1895a2ef2f54cf6582324
Catrin <catrinhughes92@gmail.com>
2020-02-04 17:31:07 +0000

    increments visitor counter by random number between one and ten

--- client/src/components/VisitorTracker.js
+++ client/src/components/VisitorTracker.js
@@ -6,11 +6,23 @@ class VisitorTracker extends Component {
     this.state = {
       visitorCount: 500
     };
+    this.addVisitors = this.addVisitors.bind(this)
   }
+
+componentDidMount() {
+  setInterval(() => this.addVisitors(), 4000)
+}
+
+addVisitors() {
+
+  this.setState(state => ({
+    visitorCount: state.visitorCount + Math.floor(Math.random() * (10 - 1) + 1)
+}))
+}
   render() {
     return (
       <div className="topTracker">
-        <p>Visitor Count:{this.state.visitorCount}</p>
+        <p>Visitor Count: {this.state.visitorCount}</p>
         <p>Days since last incident: 0</p>
       </div>
     );
