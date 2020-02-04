996c1a3828da7be6c664d3ada7c7d9d71c0e659c
Catrin <catrinhughes92@gmail.com>
2020-02-04 14:42:52 +0000

    fixed select box

--- client/src/components/dinosaurs/DinosaurCreateForm.js
+++ client/src/components/dinosaurs/DinosaurCreateForm.js
@@ -16,20 +16,14 @@ class DinosaurCreateForm extends Component {
         this.handleSelectedSex = this.handleSelectedSex.bind(this);
         this.handlePaddockChange = this.handlePaddockChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-
     }
 
     componentDidMount() {
-        const request = new Request();
-        request.get('/paddocks')
-            .then((data) => {
-                this.setState({
-                    paddocks: data._embedded.paddocks
-                })
-            })
+        this.fetchSpecies();
+        this.fetchPaddocks();
     }
 
-    componentDidMount() {
+    fetchSpecies() {
         const request = new Request();
         request.get('/species')
             .then((data) => {
@@ -37,7 +31,16 @@ class DinosaurCreateForm extends Component {
                     species: data._embedded.species
                 })
             })
-        
+    }
+
+    fetchPaddocks() {
+        const request = new Request();
+        request.get('/paddocks')
+            .then((data) => {
+                this.setState({
+                    paddocks: data._embedded.paddocks
+                })
+            })
     }
 
     handleSelectedSex(event) {
@@ -48,53 +51,64 @@ class DinosaurCreateForm extends Component {
         this.setState({ name: event.target.value })
     }
 
-    handlePaddockChange(event){
-        this.setState({selected_paddock: event.target.value})
+    handlePaddockChange(event) {
+        this.setState({ selected_paddock: event.target.value })
     }
 
     handleSubmit(event) {
         event.preventDefault();
         const newDinosaur = {
-            name: this.state.name, 
+            name: this.state.name,
             sex: this.state.sex,
             paddock: this.state.selected_paddock,
             species: this.state.selected_species._links.self.href
         }
         const request = new Request();
         request.post('/dinosaurs', newDinosaur).then(() => {
-            window.location ='/dinosaurs'
+            window.location = '/dinosaurs'
         })
     }
 
-    render(){
+    
+
+    render() {
+        const paddockList = this.state.paddocks.map((paddock, index) => {
+            return (
+                <option key={index}
+                    value={paddock._links.self.href}>
+                    {paddock.name}
+                </option>
+            )
+        })
+
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
-            <input 
-            type="text"
-            placeholder="name"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleName}
-            />
-            <input
-            type="radio"
-            name="sex"
-            value= "F"
-            onChange={this.handleSelectedSex}
-            checked={true}
-            /> Female 
+                    <input
+                        type="text"
+                        placeholder="name"
+                        name="name"
+                        value={this.state.name}
+                        onChange={this.handleName}
+                    />
+                    <input
+                        type="radio"
+                        name="sex"
+                        value="F"
+                        onChange={this.handleSelectedSex}
+                        checked={true}
+                    /> Female
              <input type="radio"
-            name="sex"
-            value= "M"
-            onChange={this.handleSelectedSex}
-            /> Male 
+                        name="sex"
+                        value="M"
+                        onChange={this.handleSelectedSex}
+                    /> Male
             <select name="paddocks" onChange={this.handlePaddockChange}>
-                {this.state.paddocks}
-            </select>
-            <button type="submit">Save new dinosaur </button>
-        </form> 
-     </div>
+                        {paddockList}
+                    </select>
+                    <button type="submit">Save new dinosaur </button>
+                </form>
+            </div>
         )
     }
 
